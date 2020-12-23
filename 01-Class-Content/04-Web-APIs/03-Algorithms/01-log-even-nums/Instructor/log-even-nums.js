b29bd837c7717d470f70d199d0b2b76bca43dac4
amaddatu <>
2020-12-22 21:42:07 -0500

    Add all today's solutions and commentary

---
+++ 01-Class-Content/04-Web-APIs/03-Algorithms/01-log-even-nums/Instructor/log-even-nums.js
@@ -0,0 +1,19 @@
+// Write code to print all even numbers from 0 to `num`
+// Assume `num` will be a positive number
+
+// num is our input parameter which is a positive integer
+var logEvenNums = function(num) {
+    // if(num === 0){
+    //     console.log(0);
+    // }
+    // if(num === 2){
+    //     console.log(0);
+    //     console.log(2);
+    // }
+    // creates an array
+    for(var i = 0; i <= num; i = i + 2){
+        // if(i % 2 === 0){
+            console.log(i);
+        // }
+    }
+};
