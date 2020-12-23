4cee30ba8d319b5165f16e649725d41b726f2042
amaddatu <>
2020-12-22 20:20:12 -0500

    Add solution

---
+++ 01-Class-Content/04-Web-APIs/01-Activities/03-Stu_Traverse-That-DOM/Instructor-2/script.js
@@ -0,0 +1,19 @@
+document.body.children[1].style.fontSize = "50px";
+
+document.body.children[1].firstElementChild.lastElementChild.style.color = "blue";
+
+document.body.children[1].previousElementSibling.style.backgroundColor = "black";
+
+document.body.children[0].childNodes[1].style.textDecoration = "underline";
+
+document.body.children[0].lastElementChild.style.fontSize = "50px";
+
+document.body.children[0].firstElementChild.style.color = "orange";
+
+document.body.children[0].lastElementChild.parentElement.style.fontSize = "40px";
+
+document.body.children[0].children[0].parentNode.style.transform = "rotateZ(5deg)";
+
+document.body.children[0].children[0].parentNode.style.fontFamily = "Courier, cursive";
+
+document.body.children[0].children[1].style.textAlign = "right";
---
+++ 01-Class-Content/04-Web-APIs/01-Activities/03-Stu_Traverse-That-DOM/Instructor/script.js
@@ -0,0 +1,19 @@
+document.body.children[1].style.fontSize = "50px";
+
+document.body.children[1].children[0].children[1].style.color = "blue";
+
+document.body.children[0].children[0].parentNode.style.backgroundColor = "black";
+
+document.body.children[0].children[1].style.textDecoration = "underline"
+
+document.body.children[0].children[1].style.fontSize = "50px"
+
+document.body.children[0].children[0].style.color = "Orange"
+
+document.body.children[0].children[0].parentNode.style.fontSize = "40px";
+
+document.body.children[0].children[0].parentNode.style.transform = "rotateZ(5deg)"
+
+document.body.children[0].children[0].parentNode.style.fontFamily = "Courier, cursive"
+
+document.body.children[0].children[0].parentNode.style.alignText = "right"
