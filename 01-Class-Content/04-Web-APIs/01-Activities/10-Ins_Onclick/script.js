215f812462543b913f6f3853e9bfdbba3fae3cf9
amaddatu <>
2021-01-05 21:42:24 -0500

    Add commentary from class

--- 01-Class-Content/04-Web-APIs/01-Activities/10-Ins_Onclick/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/10-Ins_Onclick/script.js
@@ -2,7 +2,10 @@ var themeSwitcher = document.querySelector("#theme-switcher");
 var container = document.querySelector(".container");
 var mode = "dark";
 
+// wait for a click, then run function on every click
 themeSwitcher.addEventListener("click", function() {
+  console.log("whatever i feel like goes here");
+  // conditional that uses mode for changing the look of the page
   if (mode === "dark") {
     mode = "light";
     container.setAttribute("class", "light");
