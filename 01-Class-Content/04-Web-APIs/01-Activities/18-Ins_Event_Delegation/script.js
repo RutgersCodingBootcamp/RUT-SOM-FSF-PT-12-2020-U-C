ffe56b72c5f8ed16e48615c0a54dc1a85e23fca7
amaddatu <>
2021-01-09 11:23:06 -0500

    Add notes

--- 01-Class-Content/04-Web-APIs/01-Activities/18-Ins_Event_Delegation/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/18-Ins_Event_Delegation/script.js
@@ -2,11 +2,22 @@ var listEl = document.querySelector("#grocery-list");
 var shoppingCartEl = document.querySelector("#shopping-cart");
 var groceries = ["Bananas", "Apples", "Oranges", "Grapes", "Blueberries"];
 
+var cuteList = document.querySelector("#cat-list");
+
 listEl.addEventListener("click", function(event) {
   event.preventDefault();
+  // delegation here to the button
   if(event.target.matches("button")) {
     var item = document.createElement("div");
     item.textContent = groceries[event.target.parentElement.id];
     shoppingCartEl.append(item);
+  }
+});
+
+cuteList.addEventListener("click", function(event){
+  event.preventDefault();
+  // delegation here to the button
+  if(event.target.matches("button")) {
+    alert("cat" + event.target.parentElement.id + " is cute.")
   }
 });
