095c142487a775b35397a6a869367e57f71d91fe
amaddatu <>
2021-02-09 21:00:07 -0500

    Add unpredictable

--- 01-Class-Content/09-NodeJS/01-Activities/15-Ins_Modularization/badmath.js
+++ 01-Class-Content/09-NodeJS/01-Activities/15-Ins_Modularization/badmath.js
@@ -2,8 +2,11 @@ const pie = 'apple';
 
 const predictable = () => 1;
 
+const unpredictable = () => `Peter is ${Math.random()} * 1000`;
+
 // module.exports is an object we use to store variables or methods
 module.exports = {
   pie,
   predictable,
+  unpredictable
 };
