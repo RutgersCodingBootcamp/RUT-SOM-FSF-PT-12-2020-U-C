b2c8c2ea784f8af921062b2241e3939ced3f01ce
amaddatu <>
2021-02-09 19:40:43 -0500

    Add commentary

--- 01-Class-Content/09-NodeJS/01-Activities/11-Ins_process-argv/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/11-Ins_process-argv/index.js
@@ -1,5 +1,15 @@
 // returns an array of command line arguments
-console.log(process.argv);
+// console.log(process);
+// console.log(process.argv);
 
 // arguments passed from the command line are accessed by index
-// console.log(process.argv[2]);
+console.log(process.argv[2]);
+console.log(process.argv[3]);
+console.log(process.argv[4]);
+
+let str ="node index Peter Wan Konobi";
+let strV = str.split(" ");
+
+console.log(strV[2]);
+console.log(strV[3]);
+console.log(strV[4]);
