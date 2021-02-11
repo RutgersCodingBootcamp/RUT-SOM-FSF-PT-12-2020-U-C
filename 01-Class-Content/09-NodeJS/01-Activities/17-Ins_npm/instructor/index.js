20fafcd46ab65db096dbeedab848071dbe9797af
amaddatu <>
2021-02-11 18:54:46 -0500

    Add package manager demo

---
+++ 01-Class-Content/09-NodeJS/01-Activities/17-Ins_npm/instructor/index.js
@@ -0,0 +1,15 @@
+const badmath = require('badmath');
+
+console.log(badmath.pie);
+
+console.log(badmath.predictable());
+
+// npm - NODE PACKAGE MANAGER
+
+// ** creates the package.json
+// npm init
+// ** adds a new module with package
+// npm install MODULE_NAME --save 
+
+// ** reinstall all modules if you have package.json
+// npm install
