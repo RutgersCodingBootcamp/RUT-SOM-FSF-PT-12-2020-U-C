6832744dcf564a3580ff9fc6edbffb910b86253f
amaddatu <>
2021-02-11 19:23:25 -0500

    Add demo for inquirer

--- 01-Class-Content/09-NodeJS/01-Activities/19-Ins_Inquirer-Demo/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/19-Ins_Inquirer-Demo/index.js
@@ -17,9 +17,20 @@ inquirer
       message: 'Re-enter password to confirm:',
       name: 'confirm',
     },
+    {
+      type: 'list',
+      message: 'Choose the random animal: ',
+      choices: ["Parrot", "Aligator", "Dog", "Anteater", "Penguin", "Peter"],
+      name: 'animal',
+    },
   ])
-  .then((response) =>
+  .then((response) =>{
+    console.log(response);
+    if(response.animal === "Peter"){
+      console.log("You have chosen the correct animal!!");
+    }
+
     response.confirm === response.password
       ? console.log('Success!')
       : console.log('You forgot your password already?!')
-  );
+  });
