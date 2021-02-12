dea327aa80870bc1eb9f817907dec6b10708d7e1
amaddatu <>
2021-02-11 21:26:30 -0500

    Add demo stuff

--- 01-Class-Content/09-NodeJS/01-Activities/23-Ins_Rest-and-Spread/index.js
+++ 01-Class-Content/09-NodeJS/01-Activities/23-Ins_Rest-and-Spread/index.js
@@ -1,30 +1,40 @@
 // without rest
-function add(x, y) {
-  return x + y;
-}
-console.log(add(1, 2, 3, 4, 5)); // => 3
+// function add(x, y) {
+//   return x + y;
+// }
+// console.log(add(1,2)) // => 3
+// console.log(add(1, 2, 3, 4, 5)); // => 3
 
 // 1. rest
-function add(...nums) {
-  let sum = 0;
-  for (let num of nums) sum += num;
-  return sum;
-}
+// function add(...nums) {
+//   console.log(nums);
+//   let sum = 0;
+//   for (let num of nums) sum += num;
+//   return sum;
+// }
+// console.log(add());
+// console.log(add(1)); // => 1
+// console.log(add(3, 3)); // => 6
+// console.log(add(1, 1, 4, 5)); // => 11
 
-add(1); // => 1
-add(3, 3); // => 6
-add(1, 1, 4, 5); // => 11
 
-// 2. 
-function howManyArgs(...args) {
-  return `You passed ${args.length} arguments.`; // point out the template literal
-}
+// function dummyFunc(x1, x2, x3, ...nums) {
+//   console.log(nums);
+//   return 0;
+// }
+// console.log(dummyFunc(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
 
-console.log(howManyArgs(0, 1)); // You have passed 2 arguments.
-console.log(howManyArgs("argument!", null, ["one", 2, "three"], 4)); // You have passed 4 arguments.
+// // 2. 
+// function howManyArgs(...args) {
+//   return `You passed ${args.length} arguments.`; // point out the template literal
+// }
+
+// console.log(howManyArgs(0, 1)); // You have passed 2 arguments.
+// console.log(howManyArgs("argument!", null, ["one", 2, "three"], 4)); // You have passed 4 arguments.
 
 // 1. spread
-const dragons = ["Drogon", "Viserion", "Rhaegal"];
+const petersWeapons = ["Frying pan", "AR15", "Wit", "Guitar"];
+const dragons = ["Drogon", "Viserion", "Rhaegal", ...petersWeapons];
 const weapons = ["dragonglass", ...dragons, "wildfire"]; // notice the spread operator ...dragons
 
 console.log(weapons); // prints ["dragonglass", "Drogon", "Viserion", "Rhaegal", "wildfire"]
