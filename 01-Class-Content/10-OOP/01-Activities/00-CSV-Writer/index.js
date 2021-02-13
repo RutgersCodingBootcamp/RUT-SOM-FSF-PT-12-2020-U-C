d121cabcb5708fa7874b803d492939da2e893678
amaddatu <>
2021-02-13 10:04:49 -0500

    Add warm up activity

---
+++ 01-Class-Content/10-OOP/01-Activities/00-CSV-Writer/index.js
@@ -0,0 +1,49 @@
+// goal 3 ask what the user wants for each field
+// goal 4 repeat it until they stop
+// goal 5 write it to file
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const csvQuestions = [];
+// { 
+//    type: "input",
+//    message: "",
+//    name: ""
+// }
+// goal 1 ask what the two questions are for the CSV
+// goal 2 ask what the two field names are for the CSV
+
+function askFirstQuestions(){
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your first question?",
+            name: "quest1"
+        },
+        {
+            type: "input",
+            message: "What is your first column name?",
+            name: "col1"
+        },
+        {
+            type: "input",
+            message: "What is your second question?",
+            name: "quest2"
+        },
+        {
+            type: "input",
+            message: "What is your second column name?",
+            name: "col2"
+        }
+
+    ]).then( responses => {
+        const {quest1, col1, quest2, col2} = responses;
+        console.log(responses);
+        let columnout = `"${col1}","${col2}"\n`;
+        console.log(columnout);
+        fs.writeFile("out.csv", columnout, err =>{
+            if(err) console.error(err);
+        } );
+    });
+}
+askFirstQuestions();
