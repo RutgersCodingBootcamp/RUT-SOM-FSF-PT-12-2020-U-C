7e77bc5c97cf5c2e63e012f4b4c30bd4e44b1d54
amaddatu <>
2021-02-13 10:35:28 -0500

    Add demo commentary

--- 01-Class-Content/10-OOP/01-Activities/02-Ins_Cats-And-Dogs-Constructors/rainingCatsAndDogs-con.js
+++ 01-Class-Content/10-OOP/01-Activities/02-Ins_Cats-And-Dogs-Constructors/rainingCatsAndDogs-con.js
@@ -1,7 +1,10 @@
 // Constructor function which can be used to create objects containing the properties "raining", "noise", and the "makeNoise()" function
-function Animal(raining, noise) {
+// generally, if you know the objects have different starting values, you add them to the constructor as input parameters
+function PetAnimal(raining, noise) {
   this.raining = raining;
   this.noise = noise;
+  // fat arrows maintain the context of the wrapping functional block
+  // "this" stays the same value
   this.makeNoise = () => {
     if (this.raining === true) {
       console.log(this.noise);
@@ -10,20 +13,20 @@ function Animal(raining, noise) {
 }
 
 // Sets the variables "dogs" and "cats" to be animal objects and initializes them with raining and noise properties
-const dogs = new Animal(true, "Woof!");
-const cats = new Animal(false, "Meow!");
+const dogs = new PetAnimal(true, "Woof!");
+const cats = new PetAnimal(false, "Meow!");
 
 // Calling dogs and cats makeNoise methods
-dogs.makeNoise();
-cats.makeNoise();
+dogs.makeNoise(); // Woof
+cats.makeNoise(); //
 
 // If we want, we can change an objects properties after they're created
 cats.raining = true;
-cats.makeNoise();
+cats.makeNoise(); // Meow
 
 const massHysteria = (dogs, cats) => {
   if (dogs.raining === true && cats.raining === true) {
-    console.log("DOGS AND CATS LIVING TOGETHER! MASS HYSTERIA!");
+    console.log("DOGS AND CATS LIVING TOGETHER! MASS HYSTERIA!"); // "DOGS AND CATS LIVING TOGETHER! MASS HYSTERIA!"
   }
 };
 
