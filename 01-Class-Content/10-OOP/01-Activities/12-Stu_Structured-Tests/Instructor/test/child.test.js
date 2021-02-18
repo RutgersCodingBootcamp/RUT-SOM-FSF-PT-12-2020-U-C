0a3632f586913c6514eefaa45df76027de025434
amaddatu <>
2021-02-18 18:49:46 -0500

    Add additional summary commentary

--- 01-Class-Content/10-OOP/01-Activities/12-Stu_Structured-Tests/Instructor/test/child.test.js
+++ 01-Class-Content/10-OOP/01-Activities/12-Stu_Structured-Tests/Instructor/test/child.test.js
@@ -6,17 +6,17 @@ describe("Child", () => {
     describe("Initialization", () => {
         // * If `name` is not a string or `name` is an empty string, an error is thrown.
         // This describes what we are testing and it usually includes the conditions as well
-        it('if name is not a string, like -1, an error is thrown', () => {  
+        it('should throw an error, if name is not a string, like -1', () => {  
             // Setup - create variables for the object/inputs/expected return value
             // Execute - get the results by executing the function
             // Expect - compare the expected to the results (usually using like "expect")
-            // Teardown
+            // Teardown - removes any files or reverses any changes to the system (usually not used)
             expect( () => {
                 new Child();
             }).toThrow();
         });
         // * If `name` is not a string or `name` is an empty string, an error is thrown.
-        it('if name is an empty string, like "", an error is thrown', () => {
+        it('should throw an error, if name is an empty string', () => {
             expect( () => {
                 new Child("");
             }).toThrow();
@@ -24,15 +24,32 @@ describe("Child", () => {
 
 
         // * If `age` is not a number, is `NaN` or is less than `0`, an error is thrown.
-        it('if age is not defined, an error is thrown', () => {
+        it('should throw an error, if age is not defined', () => {
             expect( () => {
                 new Child("Bob");
             }).toThrow();
         });
-        it('if age is not a number, an error is thrown', () => {
+        it('should throw an error, if age is not a number', () => {
             expect( () => {
                 new Child("Bob", "B");
             }).toThrow();
+        });
+
+        it('should have the values ("name" and "age") when we create the instance', () => {
+            // Setup
+            let inputName = "Bob";
+            let inputAge = 11;
+            let expectedName = "Bob";
+            let expectedAge = 11;
+
+            // Execute
+            let results = new Child(inputName, inputAge);
+
+            // Expect
+            expect(results.name).toEqual(expectedName);
+            expect(results.age).toEqual(expectedAge);
+
+            // Teardown
         });
     
         // * Otherwise these values are added to the created instance when the constructor is called.
