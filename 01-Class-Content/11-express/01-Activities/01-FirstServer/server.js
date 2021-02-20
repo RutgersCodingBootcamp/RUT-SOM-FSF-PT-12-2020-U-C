ed4ba089339f9cdb1557702251e7950d348e4874
amaddatu <>
2021-02-20 11:20:40 -0500

    Add some commentary

--- 01-Class-Content/11-express/01-Activities/01-FirstServer/server.js
+++ 01-Class-Content/11-express/01-Activities/01-FirstServer/server.js
@@ -2,13 +2,13 @@
 var http = require("http");
 
 // Define a port to listen for incoming requests
-var PORT = 8080;
+var PORT = 8888;
 
 // Create a generic function to handle requests and responses
 function handleRequest(request, response) {
 
   // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
+  response.end("It actually Works!! Path Hit: " + request.url);
 }
 
 // Use the Node HTTP package to create our server.
