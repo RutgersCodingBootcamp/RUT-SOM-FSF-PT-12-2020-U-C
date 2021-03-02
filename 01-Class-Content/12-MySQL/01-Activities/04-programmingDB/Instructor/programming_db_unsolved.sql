0927a03105773dbc7a98cd564eb0278d44098011
amaddatu <>
2021-03-02 18:21:13 -0500

    Add all changes?

--- 01-Class-Content/12-MySQL/01-Activities/04-programmingDB/Instructor/programming_db_unsolved.sql
+++ 01-Class-Content/12-MySQL/01-Activities/04-programmingDB/Instructor/programming_db_unsolved.sql
@@ -27,6 +27,8 @@ CREATE TABLE blog(
   language_id INTEGER NOT NULL COMMENT "same as programming_languages.id",
   website VARCHAR(50),
   
+  description TEXT,
+  
   PRIMARY KEY(blog_id)
 );
 
@@ -34,12 +36,7 @@ INSERT blog(language_id, website) VALUES (1, "https://2ality.com/");
 INSERT blog(language_id, website) VALUES (1, "https://davidwalsh.name/");
 INSERT blog(language_id, website) VALUES (4, "http://yaronschoen.com/");
 
-SELECT * FROM blog;
 
-SELECT * /*, CONCAT("<a href='", website, "'>", a.language ,"</a>") AS JSCODE */ 
-FROM programming_languages a 
-JOIN blog b 
-ON (a.id = b.language_id);
 
 -- Create new example rows
 INSERT programming_languages (language, rating)
@@ -55,3 +52,18 @@ VALUES ("HTML", 9), ("CSS", 10);
 SELECT * FROM programming_languages;
 SELECT language AS `Alias` FROM programming_languages;
 
+SELECT * FROM blog;
+
+SELECT * /*, CONCAT("<a href='", website, "'>", a.language ,"</a>") AS JSCODE */ 
+FROM programming_languages a 
+INNER JOIN blog b 
+ON (a.id = b.language_id)
+WHERE description IS NOT NULL;
+
+UPDATE blog 
+SET description = "Awesome blog made by someone not named Peter"
+WHERE blog_id = 1;
+
+UPDATE blog 
+SET description = "Awesome blog made by someone not named Peter"
+WHERE language_id = 1;
--- 01-Class-Content/12-MySQL/01-Activities/05-booksDB/books.sql
+++ 01-Class-Content/12-MySQL/01-Activities/05-booksDB/books.sql
@@ -37,14 +37,16 @@ SELECT title, firstName, lastName
 FROM books
 INNER JOIN authors ON books.authorId = authors.id;
 
+
+SELECT * FROM authors;
+SELECT * FROM books;
 -- show ALL books, even if we don't know the author
 -- LEFT JOIN returns all of the values from the left table, and the matching ones from the right table
 SELECT title, firstName, lastName
-FROM books
-LEFT JOIN authors ON books.authorId = authors.id;
+FROM books LEFT JOIN authors ON books.authorId = authors.id;
 
 -- show ALL books, even if we don't know the author
 -- RIGHT JOIN returns all of the values from the right table, and the matching ones from the left table
 SELECT title, firstName, lastName
-FROM books
-RIGHT JOIN authors ON books.authorId = authors.id;
+FROM books RIGHT JOIN authors 
+ON books.authorId = authors.id;
