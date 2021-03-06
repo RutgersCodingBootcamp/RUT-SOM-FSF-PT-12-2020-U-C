bb556076ac53ba6f72be375d22570ed8e77e658b
amaddatu <>
2021-03-06 10:24:47 -0500

    Add commentary and answers

--- 01-Class-Content/12-MySQL/01-Activities/13-Top5000Code/Instructor/queries.sql
+++ 01-Class-Content/12-MySQL/01-Activities/13-Top5000Code/Instructor/queries.sql
@@ -12,7 +12,19 @@ GROUP BY artist
 HAVING COUNT(*) > 1 
 ORDER BY `number` DESC;
 
+-- advanced search for ranges
+SELECT * FROM top5000 WHERE position BETWEEN 10 AND 100;
+SELECT * FROM top5000 WHERE release_year BETWEEN 2000 AND 2100;
+-- pagination query - offset, rows
+SELECT * FROM top5000 LIMIT 100,10;
 
+-- general search - basic blogs, basic site searches
+SELECT * FROM top5000 WHERE "Prince" IN (artist,title,release_year,position);
+SELECT * FROM top5000 WHERE 2000 IN (artist,title,release_year,position);
+
+-- advanced search - DBA write these for more advanced apps
+SELECT * FROM top5000 WHERE release_year = 2000;
+SELECT * FROM top5000 WHERE release_year IN (2000, 2001, 2011);
 
 --    * A query which returns all data contained within a specific range
 --  * A query which searches for a specific song in the top 5000 and returns the data for it
