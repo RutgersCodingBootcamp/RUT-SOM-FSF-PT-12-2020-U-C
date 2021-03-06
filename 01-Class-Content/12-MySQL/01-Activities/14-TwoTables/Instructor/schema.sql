6ec10b039fbba6e799c0fdb6a6489a21d2737fe2
amaddatu <>
2021-03-06 10:35:20 -0500

    Add schema

---
+++ 01-Class-Content/12-MySQL/01-Activities/14-TwoTables/Instructor/schema.sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS top_songsDB2;
+CREATE database top_songsDB2;
+
+USE top_songsDB2;
+
+
+CREATE TABLE top1000songs (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE top_albums (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+CREATE TABLE top_songs (
+  position INT NOT NULL,
+  artist VARCHAR(100) NULL,
+  song VARCHAR(100) NULL,
+  year INT NULL,
+  raw_total DECIMAL(10,4) NULL,
+  raw_usa DECIMAL(10,4) NULL,
+  raw_uk DECIMAL(10,4) NULL,
+  raw_eur DECIMAL(10,4) NULL,
+  raw_row DECIMAL(10,4) NULL,
+  PRIMARY KEY (position)
+);
+
+SELECT * FROM top1000songs;
+
+SELECT * FROM top_albums;
+
+SELECT * FROM top_songs;
