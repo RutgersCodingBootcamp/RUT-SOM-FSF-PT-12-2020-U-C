0bd0750bb93e6408d09bea606e253abb22bc2d83
amaddatu <>
2021-03-11 21:06:54 -0500

    Add commentary with SQL queries

--- 01-Class-Content/13-MVC/01-Activities/12-OrmExample/config/orm.js
+++ 01-Class-Content/13-MVC/01-Activities/12-OrmExample/config/orm.js
@@ -9,6 +9,7 @@ var connection = require("./connection.js");
 var orm = {
   selectWhere: function(tableInput, colToSearch, valOfCol) {
     var queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    // SELECT * FROM pets WHERE animal_name = "Rachel"
     connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
       if (err) throw err;
       console.log(result);
@@ -16,6 +17,7 @@ var orm = {
   },
   selectAndOrder: function(whatToSelect, table, orderCol) {
     var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+    // SELECT animal_name FROM pets ORDER BY price DESC;
     console.log(queryString);
     connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
       if (err) throw err;
@@ -25,7 +27,8 @@ var orm = {
   findWhoHasMost: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
     var queryString =
       "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
-
+    // SELECT buyer_name, COUNT(buyer_name) AS count FROM buyers LEFT JOIN pets ON pets.buyer_id= buyers.id GROUP BY buyer_name ORDER BY count DESC LIMIT 1"
+    
     connection.query(
       queryString,
       [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
--- 01-Class-Content/13-MVC/01-Activities/12-OrmExample/server.js
+++ 01-Class-Content/13-MVC/01-Activities/12-OrmExample/server.js
@@ -4,7 +4,7 @@ var orm = require("./config/orm.js");
 orm.selectAndOrder("animal_name", "pets", "price");
 
 // Find a pet in the pets table by an animal_name of Rachel.
-orm.selectWhere("pets", "animal_name", "Rachel");
+orm.selectWhere("pets", "animal_name", "Jake");
 
 // Find the buyer with the most pets.
 orm.findWhoHasMost("buyer_name", "buyer_id", "buyers", "pets");
