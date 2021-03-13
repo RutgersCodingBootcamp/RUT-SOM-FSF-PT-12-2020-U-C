756240da5d30e2bb4282c219ab228623d3486e69
amaddatu <>
2021-03-13 11:35:35 -0500

    Add commentary

--- 01-Class-Content/13-MVC/01-Activities/14-TheAsyncProblem/Unsolved/config/orm.js
+++ 01-Class-Content/13-MVC/01-Activities/14-TheAsyncProblem/Unsolved/config/orm.js
@@ -1,13 +1,18 @@
 var connection = require("../config/connection.js");
 
+
 var orm = {
   selectWhere: function(tableInput, colToSearch, valOfCol) {
     var queryString = "SELECT * FROM ?? WHERE ?? = ?";
 
-    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+    return connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
       if (err) throw err;
+      // console.log(result);
       return result;
     });
+
+    // if we dont put a return... return undefined
+    // return undef
   }
 };
 
