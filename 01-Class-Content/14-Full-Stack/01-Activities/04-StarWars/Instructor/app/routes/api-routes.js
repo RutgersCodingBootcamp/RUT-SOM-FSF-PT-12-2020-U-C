b97dc7eec9bc91a58070f61bb2d4e0fc1e43f71b
amaddatu <>
2021-03-16 21:37:54 -0400

    Add starwars solutions

---
+++ 01-Class-Content/14-Full-Stack/01-Activities/04-StarWars/Instructor/app/routes/api-routes.js
@@ -0,0 +1,82 @@
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+
+const Character = require("../models/Character");
+
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Search for Specific Character (or all characters) then provides JSON
+  // the question mark means the wildcard parameter is optional
+  app.get("/api/:characters?", function(req, res) {
+
+    // if Character..
+    if(req.params.characters){
+      Character.findOne({
+        where: {
+          routeName: req.params.characters
+        }
+      }).then( data => {
+        res.json(data);
+      });
+      // res.json({
+      //   name: "Peter Obi Wan Kenobi",
+      //   role: "Jedi TA",
+      //   age: 1000,
+      //   forcePoints: 10000
+      // });
+    }
+      // then single Object
+    else{
+    // else
+      Character.findAll({}).then( data => {
+        res.json(data);
+      });
+      //   array of objects
+      // res.json([{
+      //   name: "Peter Obi Wan Kenobi",
+      //   role: "Jedi TA",
+      //   age: 1000,
+      //   forcePoints: 10000
+      // }]);
+    }
+      // // If the user provides a specific character in the URL...
+    // if (req.params.characters) {
+
+    //   // Then display the JSON for ONLY that character.
+    //   // (Note how we're using the ORM here to run our searches)
+    //   orm.searchCharacter(req.params.characters, function(data) {
+    //     res.json(data);
+    //   });
+    // }
+
+    // // Otherwise...
+    // else {
+    //   // Otherwise display the data for all of the characters.
+    //   // (Note how we're using the ORM here to run our searches)
+    //   orm.allCharacters(function(data) {
+    //     res.json(data);
+    //   });
+    // }
+
+  });
+
+  // If a user sends data to add a new character...
+  app.post("/api/new", function(req, res) {
+
+    // // Take the request...
+    // var character = req.body;
+
+    // // Then send it to the ORM to "save" into the DB.
+    // orm.addCharacter(character, function(data) {
+    //   console.log(data);
+    // });
+
+  });
+};
