8afd4c8f8283b76966051d411da02c5e187a28b2
amaddatu <>
2021-04-15 21:28:07 -0400

    Add demo

--- 01-Class-Content/18-PWA/01-Activities/10-Ins_Service_Workers/public/service-worker.js
+++ 01-Class-Content/18-PWA/01-Activities/10-Ins_Service_Workers/public/service-worker.js
@@ -1,1 +1,5 @@
-console.log("Hello from service worker!")
+console.log("Peter and the Pineapple - A short story by Bob the Builder");
+// application -> storage -> clear site data
+// manage cache
+// manage API requests
+// manage Service requests ("backlog and replay")
---
+++ 01-Class-Content/18-PWA/01-Activities/11-Stu_Service_Workers/Instructor/public/service-pineapple.js
@@ -0,0 +1,1 @@
+console.log("this is my service pineapple (service worker)");
