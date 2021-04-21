52d0b6b9de9e264f39dea63b5784110c1bf76a06
amaddatu <>
2021-04-20 21:38:54 -0400

    Add commentary and notes

---
+++ 01-Class-Content/19-React/01-Activities/03-Stu_HelloDiv/Instructor/src/components/HelloDiv.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+
+function HelloDiv() {
+  return (
+    <Fragment>
+      <h1>Peter needs better jokes.</h1>
+      <ul>
+        <li>Why did the chicken cross the road? To avoid the flying mango.</li>
+        <li>Your momma is so thin, because she loves to eat mangoes.</li>
+        <li>Them mango trees be shady.</li>
+      </ul>
+    </Fragment>
+  );
+} 
+
+export default HelloDiv;
