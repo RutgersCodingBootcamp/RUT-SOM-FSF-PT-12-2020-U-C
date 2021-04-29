972cdf4f74271a3d08831a97e377c033c4e362e7
amaddatu <>
2021-04-29 19:53:35 -0400

    Add pupster demo

---
+++ 01-Class-Content/19-React/01-Activities/23-Stu_PupsterApp/Instructor/src/pages/about/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About(){
+    return (
+        <>
+            <h1>About Pupster</h1>
+            <p>This app is about puppies. Peter likes puppies but hates coding bugs.</p>
+        </>
+    );
+}
+
+export default About;
