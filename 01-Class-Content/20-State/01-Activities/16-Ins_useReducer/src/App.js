704facdc1e404cad848500778f2a7a8610601e30
amaddatu <>
2021-05-04 21:35:32 -0400

    Add demo code and project requirements

--- 01-Class-Content/20-State/01-Activities/16-Ins_useReducer/src/App.js
+++ 01-Class-Content/20-State/01-Activities/16-Ins_useReducer/src/App.js
@@ -2,15 +2,16 @@ import React, { useReducer } from "react";
 import "./App.css";
 
 function App() {
+  // reducers are naturally pure functions, you should be using only the inputs from the reducer to prevent side effects
   const [count, dispatch] = useReducer((state, action) => {
     if (action === "add") {
-      return state + 1;
+      return state + 1; // the return value is the new state (+1)
     } else if (action === "subtract") {
-      return state - 1;
+      return state - 1; // the return value is the new state (-1)
     } else {
-      return state;
+      return state; // returning state means no change...
     }
-  }, 0);
+  }, 0); // 0 is default state
 
   return (
     <div className="App">
@@ -20,7 +21,7 @@ function App() {
       <div>{count}</div>
       <button className="btn btn-danger mt-5" onClick={() => dispatch("subtract")}>
         Subtract
-      </button>
+       </button>
     </div>
   );
 }
